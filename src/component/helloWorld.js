8ab7cdfae752826a63bef7289d86c63ca444c419
huaj-todd <toddhua1994@gmail.com>
2022-11-10 20:46:04 -0500

    add list.js and motify this js file

---
+++ src/component/helloWorld.js
@@ -0,0 +1,11 @@
+import React from "react";
+//import {Link} from "react-router-dom";
+const HelloWorld = () => {
+    return(
+        <>
+            <h1>Hello World!</h1>
+            <img src="https://improvephotography.com/wp-content/uploads/2017/07/DSCF5660-Edit-1.jpg" alt="alternatetext"/>
+        </>
+    );
+}
+export default HelloWorld;
