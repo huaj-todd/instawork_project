30a6da9d1c679b788cc8a4452c8d63cbaee61e2f
huaj-todd <toddhua1994@gmail.com>
2022-11-12 22:24:28 -0500

    something

--- src/App.js
+++ src/App.js
@@ -7,19 +7,15 @@ import './vendor/PageCSS.css'
 import ListScreen from "./component/ListScreen/List";
 import AddScreen from "./component/AddScreen/Add";
 
-
-import memberReducer from "./component/reducer/memberReducer";
-import {combineReducers, createStore} from 'redux';
-import { Provider } from "react-redux";
 import React from 'react';
 import {BrowserRouter, Route,Routes} from "react-router-dom";
 
-const reducer = combineReducers({member:memberReducer});
-const store = createStore(reducer);
+
+
 
 function App() {
   return (
-      <Provider store={store}>
+      <>
           <BrowserRouter>
               <div className="container">
                   <Routes>
@@ -33,7 +29,7 @@ function App() {
 
               </div>
           </BrowserRouter>
-      </Provider>
+      </>
 
   );
 }
--- src/component/ListScreen/List.js
+++ src/component/ListScreen/List.js
@@ -1,35 +1,44 @@
 import React from "react";
 import ListItem from "./ListItem";
 import {useSelector} from "react-redux";
+import memberReducer from "../reducer/memberReducer";
+import {combineReducers} from 'redux';
+import { legacy_createStore as createStore } from 'redux'
+import { Provider } from "react-redux";
+
+
+const reducer = combineReducers(memberReducer);
+const newStore = createStore(reducer);
 
 const ListScreen  = () =>{
     const members = useSelector(state=>state.members)
     return(
-        <>
-            <div className={'row'}>
-                <div className={'col-11'}>
-                    <h1>Team Member</h1>
-                    <h5 className={'wd-member'}>We have {Object.keys(members).length} members</h5>
+
+        <Provider store={newStore}>
+                <div className={'row'}>
+                    <div className={'col-11'}>
+                        <h1>Team Member</h1>
+                        <h5 className={'wd-member'}>We have {Object.keys(members).length} members</h5>
+                    </div>
+                    <div className={'col-1'}>
+                        <a href={'/add'}>
+                            <button type="button" className={'btn btn-light wd-round-btn'}>
+                                <i className={"fa-solid fa-plus fa-2xl wd-icon-style"}/>
+                            </button>
+                        </a>
+
+
+                    </div>
                 </div>
-                <div className={'col-1'}>
-                    <a href={'/add'}>
-                        <button type="button" className={'btn btn-light wd-round-btn'}>
-                            <i className={"fa-solid fa-plus fa-2xl wd-icon-style"}/>
-                        </button>
-                    </a>
-
-
+                <div className={'list-group'}>
+                    {
+                        members.map(member =>{
+                            return(<ListItem member = {member}/>);
+                        })
+                    }
                 </div>
-            </div>
-            <div className={'list-group'}>
-                {
-                    members.map(member =>{
-                        return(<ListItem member = {member}/>);
-                    })
-                }
+        </Provider>
 
-            </div>
-        </>
-    )
-}
+    );
+};
 export  default  ListScreen
